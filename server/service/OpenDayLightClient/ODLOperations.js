15a9f4a82f7c94fb85db93b2e7cc22b45d9ee01f
Prathiba <prathiba.jeevan.external@telefonica.com>
2023-12-07 10:33:10 +0100

    To implement a cyclic process to load the DeviceList From Controller Fixes #31

--- server/service/OpenDayLightClient/ODLOperations.js
+++ server/service/OpenDayLightClient/ODLOperations.js
@@ -34,11 +34,6 @@ exports.writeToLive = async function (requestBody, forwardingName, pathParams, s
     return responseCode;
 }
 
-
-
-
-
-
 /**
  * This function performs PUT request to the ODL controller
  * @param {object} requestBody httpRequestBody to be sent to the ODL controller
@@ -69,6 +64,36 @@ exports.postToLive = async function (requestBody, forwardingName, pathParams, st
     return responseCode;
 }
 
+
+/**
+ * This function performs PUT request to the ODL controller
+ * @param {object} requestBody httpRequestBody to be sent to the ODL controller
+ * @param {String} forwardingName Name of the forwardingConstruct
+ * @param {String} pathParams inline path parameters to be sent in the ODL request
+ * @param {String} stringProfileName stringProfile name of the field filter 
+ * @param {object} eatlRequestHeaders httpRequestHeader to be sent to record the service information
+ * @returns {Integer} responseCode httpResponseCode received from the controller
+ */
+exports.readFromLive = async function (requestBody, forwardingName, pathParams, stringProfileName, eatlRequestHeaders) {
+    let responseCode;
+    try {
+        let consequentOperationClient = await RESTUtility.getConsequentOperationClientAndFieldParams(forwardingName, stringProfileName);
+        responseCode = await ODLClient.prepareAndSendRequestToODL(
+            consequentOperationClient,
+            pathParams,
+            requestBody,
+            eatlRequestHeaders,
+            "GET");
+    } catch (error) {
+        console.log(`${forwardingName} is not success with ${error}`);
+        if (error == "Not Found") {
+            throw new createHttpError.NotFound();
+        } else {
+            return new createHttpError.InternalServerError();
+        }
+    }
+    return responseCode;
+}
 
 
 
